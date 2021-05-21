8ce89070938f91afc0573269c19f07da1f5dda24
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2021-05-21 19:27:28 +0530

    input preprocessing done

--- backend/api/preprocess.py
+++ backend/api/preprocess.py
@@ -1,2 +1,186 @@
-def preprocess(answer):
-    return "dfsafd"
+# importing required packages
+
+import nltk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+import re, collections
+from collections import defaultdict
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.svm import SVR
+from sklearn import ensemble
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import cohen_kappa_score
+def sentence_to_wordlist(raw_sentence):
+    
+    clean_sentence = re.sub("[^a-zA-Z0-9]"," ", raw_sentence)
+    tokens = nltk.word_tokenize(clean_sentence)
+    
+    return tokens
+# tokenizing an essay into a list of word lists
+
+def tokenize(essay):
+    stripped_essay = essay.strip()
+    
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    raw_sentences = tokenizer.tokenize(stripped_essay)
+    
+    tokenized_sentences = []
+    for raw_sentence in raw_sentences:
+        if len(raw_sentence) > 0:
+            tokenized_sentences.append(sentence_to_wordlist(raw_sentence))
+    
+    return tokenized_sentences
+def avg_word_len(essay):
+    
+    clean_essay = re.sub(r'\W', ' ', essay)
+    words = nltk.word_tokenize(clean_essay)
+    
+    return sum(len(word) for word in words) / len(words)
+def word_count(essay):
+    
+    clean_essay = re.sub(r'\W', ' ', essay)
+    words = nltk.word_tokenize(clean_essay)
+    
+    return len(words)
+def char_count(essay):
+    
+    clean_essay = re.sub(r'\s', '', str(essay).lower())
+    
+    return len(clean_essay)
+def sent_count(essay):
+    
+    sentences = nltk.sent_tokenize(essay)
+    
+    return len(sentences)
+# calculating number of lemmas per essay
+
+def count_lemmas(essay):
+    
+    tokenized_sentences = tokenize(essay)      
+    
+    lemmas = []
+    wordnet_lemmatizer = WordNetLemmatizer()
+    
+    for sentence in tokenized_sentences:
+        tagged_tokens = nltk.pos_tag(sentence) 
+        
+        for token_tuple in tagged_tokens:
+        
+            pos_tag = token_tuple[1]
+        
+            if pos_tag.startswith('N'): 
+                pos = wordnet.NOUN
+                lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
+            elif pos_tag.startswith('J'):
+                pos = wordnet.ADJ
+                lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
+            elif pos_tag.startswith('V'):
+                pos = wordnet.VERB
+                lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
+            elif pos_tag.startswith('R'):
+                pos = wordnet.ADV
+                lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
+            else:
+                pos = wordnet.NOUN
+                lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
+    
+    lemma_count = len(set(lemmas))
+    
+    return lemma_count
+def count_spell_error(essay):
+    
+    clean_essay = re.sub(r'\W', ' ', str(essay).lower())
+    clean_essay = re.sub(r'[0-9]', '', clean_essay)
+    
+    #big.txt: It is a concatenation of public domain book excerpts from Project Gutenberg 
+    #         and lists of most frequent words from Wiktionary and the British National Corpus.
+    #         It contains about a million words.
+    data = open('big.txt').read()
+    
+    words_ = re.findall('[a-z]+', data.lower())
+    
+    word_dict = collections.defaultdict(lambda: 0)
+                       
+    for word in words_:
+        word_dict[word] += 1
+                       
+    clean_essay = re.sub(r'\W', ' ', str(essay).lower())
+    clean_essay = re.sub(r'[0-9]', '', clean_essay)
+                        
+    mispell_count = 0
+    
+    words = clean_essay.split()
+                        
+    for word in words:
+        if not word in word_dict:
+            mispell_count += 1
+    
+    return mispell_count
+def count_pos(essay):
+    
+    tokenized_sentences = tokenize(essay)
+    
+    noun_count = 0
+    adj_count = 0
+    verb_count = 0
+    adv_count = 0
+    
+    for sentence in tokenized_sentences:
+        tagged_tokens = nltk.pos_tag(sentence)
+        
+        for token_tuple in tagged_tokens:
+            pos_tag = token_tuple[1]
+        
+            if pos_tag.startswith('N'): 
+                noun_count += 1
+            elif pos_tag.startswith('J'):
+                adj_count += 1
+            elif pos_tag.startswith('V'):
+                verb_count += 1
+            elif pos_tag.startswith('R'):
+                adv_count += 1
+            
+    return noun_count, adj_count, verb_count, adv_count
+def get_count_vectors(essays):
+    
+    vectorizer = CountVectorizer(max_features = 10000, ngram_range=(1, 3), stop_words='english')
+    
+    count_vectors = vectorizer.fit_transform(essays)
+    
+    feature_names = vectorizer.get_feature_names()
+    
+    return feature_names, count_vectors
+
+def Preprocessing():
+    dataframe = pd.read_csv('essays_and_scores.csv', encoding = 'latin-1')
+    # getting relevant columns
+
+    data = dataframe[['essay_set','essay','domain1_score']].copy()
+    
+    print(data)
+    feature_names_cv, count_vectors = get_count_vectors(data[data['essay_set'] == 1]['essay'])
+
+    X_cv = count_vectors.toarray()
+
+    y_cv = data[data['essay_set'] == 1]['domain1_score']
+    X_train, X_test, y_train, y_test = train_test_split(X_cv, y_cv, test_size = 0.3)
+    return (X_train,X_test,y_train,y_test)
+def process_input(text):
+    dataframe = pd.read_csv('http://127.0.0.1:8000/static/essays.csv', encoding = 'latin-1')
+    # getting relevant columns
+    
+    data = dataframe[['essay_set','essay','domain1_score']].copy()
+    
+    vectorizer = CountVectorizer(max_features = 10000, ngram_range=(1, 3), stop_words='english')
+    
+    count_vectors = vectorizer.fit_transform(data[data['essay_set'] == 1]['essay'])
+    
+    text= vectorizer.transform([text])
+    text = text.toarray()
+    return text
--- urls/api/views.py
+++ urls/api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
-from backend.api.preprocess import preprocess
+from backend.api.preprocess import process_input
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
 class AnswerForm(forms.Form):
    answer = forms.CharField(max_length = 100)
    
@@ -14,8 +17,8 @@ def input(request):
         if answer.is_valid():
             answer = answer.cleaned_data['answer']
         #########TODO#########################################################################################
-        pre_answer= preprocess(answer)
-        
+        pre_answer= process_input(answer)
+        print(pre_answer)
         marks = predict(pre_answer)
 
 
@@ -23,16 +26,16 @@ def input(request):
 
 
 
-        return HttpResponse(marks)
+        return HttpResponse(pre_answer)
         
     return render(request,"form.html")
 
 def predict(request):
     ################Composite Algorithm prediction###################
 
-    # include preprocessing in composite algorithm part itself
-    #just return prediction marks
-    prediction = request
     
-    return prediction
+    #just return prediction marks
+    prediction = "Marks predicted is  5"
+    
+    return prediction,
     
