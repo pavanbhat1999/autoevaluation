82659caeb51d2514dea79e3ab6efc8fe0535708a
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2021-05-22 11:07:09 +0530

    url redirection added

--- AutoEval/urls.py
+++ AutoEval/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import RedirectView
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/input/', permanent=True)),
     path('input/',include('urls.api.urls')),
    
 ]
