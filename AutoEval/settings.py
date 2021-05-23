25fe25015d8f9208363967f25e621f65c1e0b494
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2021-05-23 11:02:00 +0530

    changed for static

--- AutoEval/settings.py
+++ AutoEval/settings.py
@@ -128,14 +128,14 @@ USE_TZ = True
     # '/var/www/static/',
 # ]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
