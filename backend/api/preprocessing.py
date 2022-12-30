707ed4c1e299f58f14968587f36ddaae061b2326
pavanbhat19999 <prbhat07@gmail.com>
2022-12-30 18:00:19 +0530

    make it better

--- backend/api/preprocessing.py
+++ backend/api/preprocessing.py
@@ -17,64 +17,64 @@ from sklearn import ensemble
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import cohen_kappa_score
 def sentence_to_wordlist(raw_sentence):
-    
+
     clean_sentence = re.sub("[^a-zA-Z0-9]"," ", raw_sentence)
     tokens = nltk.word_tokenize(clean_sentence)
-    
+
     return tokens
 # tokenizing an essay into a list of word lists
 
 def tokenize(essay):
     stripped_essay = essay.strip()
-    
+
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
     raw_sentences = tokenizer.tokenize(stripped_essay)
-    
+
     tokenized_sentences = []
     for raw_sentence in raw_sentences:
         if len(raw_sentence) > 0:
             tokenized_sentences.append(sentence_to_wordlist(raw_sentence))
-    
+
     return tokenized_sentences
 def avg_word_len(essay):
-    
+
     clean_essay = re.sub(r'\W', ' ', essay)
     words = nltk.word_tokenize(clean_essay)
-    
+
     return sum(len(word) for word in words) / len(words)
 def word_count(essay):
-    
+
     clean_essay = re.sub(r'\W', ' ', essay)
     words = nltk.word_tokenize(clean_essay)
-    
+
     return len(words)
 def char_count(essay):
-    
+
     clean_essay = re.sub(r'\s', '', str(essay).lower())
-    
+
     return len(clean_essay)
 def sent_count(essay):
-    
+
     sentences = nltk.sent_tokenize(essay)
-    
+
     return len(sentences)
 # calculating number of lemmas per essay
 
 def count_lemmas(essay):
-    
-    tokenized_sentences = tokenize(essay)      
-    
+
+    tokenized_sentences = tokenize(essay)
+
     lemmas = []
     wordnet_lemmatizer = WordNetLemmatizer()
-    
+
     for sentence in tokenized_sentences:
-        tagged_tokens = nltk.pos_tag(sentence) 
-        
+        tagged_tokens = nltk.pos_tag(sentence)
+
         for token_tuple in tagged_tokens:
-        
+
             pos_tag = token_tuple[1]
-        
-            if pos_tag.startswith('N'): 
+
+            if pos_tag.startswith('N'):
                 pos = wordnet.NOUN
                 lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
             elif pos_tag.startswith('J'):
@@ -89,55 +89,55 @@ def count_lemmas(essay):
             else:
                 pos = wordnet.NOUN
                 lemmas.append(wordnet_lemmatizer.lemmatize(token_tuple[0], pos))
-    
+
     lemma_count = len(set(lemmas))
-    
+
     return lemma_count
 def count_spell_error(essay):
-    
+
     clean_essay = re.sub(r'\W', ' ', str(essay).lower())
     clean_essay = re.sub(r'[0-9]', '', clean_essay)
-    
-    #big.txt: It is a concatenation of public domain book excerpts from Project Gutenberg 
+
+    #big.txt: It is a concatenation of public domain book excerpts from Project Gutenberg
     #         and lists of most frequent words from Wiktionary and the British National Corpus.
     #         It contains about a million words.
     data = open('big.txt').read()
-    
+
     words_ = re.findall('[a-z]+', data.lower())
-    
+
     word_dict = collections.defaultdict(lambda: 0)
-                       
+
     for word in words_:
         word_dict[word] += 1
-                       
+
     clean_essay = re.sub(r'\W', ' ', str(essay).lower())
     clean_essay = re.sub(r'[0-9]', '', clean_essay)
-                        
+
     mispell_count = 0
-    
+
     words = clean_essay.split()
-                        
+
     for word in words:
         if not word in word_dict:
             mispell_count += 1
-    
+
     return mispell_count
 def count_pos(essay):
-    
+
     tokenized_sentences = tokenize(essay)
-    
+
     noun_count = 0
     adj_count = 0
     verb_count = 0
     adv_count = 0
-    
+
     for sentence in tokenized_sentences:
         tagged_tokens = nltk.pos_tag(sentence)
-        
+
         for token_tuple in tagged_tokens:
             pos_tag = token_tuple[1]
-        
-            if pos_tag.startswith('N'): 
+
+            if pos_tag.startswith('N'):
                 noun_count += 1
             elif pos_tag.startswith('J'):
                 adj_count += 1
@@ -145,37 +145,37 @@ def count_pos(essay):
                 verb_count += 1
             elif pos_tag.startswith('R'):
                 adv_count += 1
-            
+
     return noun_count, adj_count, verb_count, adv_count
 def extract_features(data):
-    
+
     features = data.copy()
-    
+
     features['char_count'] = features['essay'].apply(char_count)
-    
+
     features['word_count'] = features['essay'].apply(word_count)
-    
+
     features['sent_count'] = features['essay'].apply(sent_count)
-    
+
     features['avg_word_len'] = features['essay'].apply(avg_word_len)
-    
+
     features['lemma_count'] = features['essay'].apply(count_lemmas)
-    
+
     #features['spell_err_count'] = features['essay'].apply(count_spell_error)
-    
+
     features['noun_count'], features['adj_count'], features['verb_count'], features['adv_count'] = zip(*features['essay'].map(count_pos))
-    
+
     return features
 
 
 def get_count_vectors(essays):
-    
+
     vectorizer = CountVectorizer(max_features = 10000, ngram_range=(1, 3), stop_words='english')
-    
+
     count_vectors = vectorizer.fit_transform(essays)
-    
+
     feature_names = vectorizer.get_feature_names()
-    
+
     return feature_names, count_vectors
 def fit_count_vectors(essays,answer):
     vectorizer = pickle.load(open('static/text.sav','rb'))
@@ -203,9 +203,9 @@ def fit_count_vectors(essays,answer):
 def preprocessing(answer=None,isTest=False):
     dataframe = pd.read_csv('static/essays.csv', encoding = 'latin-1')
     data = dataframe[['essay_set','essay','domain1_score']].copy()
-    
+
     if(isTest):
-        
+
         new_data = [[1,answer]]
         new_data = pd.DataFrame(new_data,columns=['essay_set','essay'])
         print("Extracting Features of given answer.....")
@@ -219,7 +219,7 @@ def preprocessing(answer=None,isTest=False):
     else:
         print("calculating Vectors : ")
         feature_names_cv, count_vectors = get_count_vectors(data[data['essay_set'] == 1]['essay'])
-    
+
         X_cv = count_vectors.toarray()  # X without features only BOW
         y_cv = data[data['essay_set'] == 1]['domain1_score']
         print(y_cv.shape)
@@ -246,9 +246,9 @@ def preprocessing(answer=None,isTest=False):
 def preprocessing_features(answer=None,isTest=False):
     dataframe = pd.read_csv('static/essays.csv', encoding = 'latin-1')
     data = dataframe[['essay_set','essay','domain1_score']].copy()
-    
+
     if(isTest):
-        
+
         new_data = [[1,answer]]
         new_data = pd.DataFrame(new_data,columns=['essay_set','essay'])
         print("Extracting Features of given answer.....")
@@ -262,7 +262,7 @@ def preprocessing_features(answer=None,isTest=False):
     else:
         # print("calculating Vectors : ")
         # feature_names_cv, count_vectors = get_count_vectors(data[data['essay_set'] == 1]['essay'])
-    
+
         # X_cv = count_vectors.toarray()  # X without features only BOW
         y_cv = data[data['essay_set'] == 1]['domain1_score']
         print(y_cv.shape)
